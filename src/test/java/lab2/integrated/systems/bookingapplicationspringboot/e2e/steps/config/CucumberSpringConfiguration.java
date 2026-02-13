878dcf9129f6e9dfa1cf0f030e95bb00948ff76b
kristijanjovcevski <96358073+kristijanjovcevski@users.noreply.github.com>
2026-02-13 22:30:56 +0100

    Fix annotations and update PostgreSQL version

--- src/test/java/lab2/integrated/systems/bookingapplicationspringboot/e2e/steps/config/CucumberSpringConfiguration.java
+++ src/test/java/lab2/integrated/systems/bookingapplicationspringboot/e2e/steps/config/CucumberSpringConfiguration.java
@@ -3,22 +3,22 @@ package lab2.integrated.systems.bookingapplicationspringboot.e2e.steps.config;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@TestContainers
+@Testcontainers
 @CucumberContextConfiguration
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
 
-        @Container
-        static PostgreSQLContainer<?> postgres = new PostgresSQLContainer<>("postgres:17")
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16")
                     .withDatabaseName("testdb")
                     .withUsername("test")
                     .withPassword("test");
 
-        @DynamicPropertySource
-        static void configureProperties(DynamicPropertySource registry) {
-                registry.add("spring.datasource.url", postgres::getJdbcUrl);
-                registry.add("spring.datasource.username", postgres::getUsername);
-                registry.add("spring.datasource.password", postgres::getPassword);
-        }
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 }
