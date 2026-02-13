0d5f1c7c3862918c699ac15c6d266e8a07ca9c76
kristijanjovcevski <96358073+kristijanjovcevski@users.noreply.github.com>
2026-02-13 22:23:46 +0100

    Configure Cucumber tests with PostgreSQL test container
    Updated the web environment to use RANDOM_PORT and added PostgreSQL container configuration for testing.

--- src/test/java/lab2/integrated/systems/bookingapplicationspringboot/e2e/steps/config/CucumberSpringConfiguration.java
+++ src/test/java/lab2/integrated/systems/bookingapplicationspringboot/e2e/steps/config/CucumberSpringConfiguration.java
@@ -3,8 +3,22 @@ package lab2.integrated.systems.bookingapplicationspringboot.e2e.steps.config;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@TestContainers
 @CucumberContextConfiguration
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
+
+        @Container
+        static PostgreSQLContainer<?> postgres = new PostgresSQLContainer<>("postgres:17")
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+        @DynamicPropertySource
+        static void configureProperties(DynamicPropertySource registry) {
+                registry.add("spring.datasource.url", postgres::getJdbcUrl);
+                registry.add("spring.datasource.username", postgres::getUsername);
+                registry.add("spring.datasource.password", postgres::getPassword);
+        }
 }
